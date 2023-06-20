ab2c1418dedf918f768840d5de56f5ca3b30b44e
Ivan Soldatov <131395398+ISoldatov@users.noreply.github.com>
2023-06-20 15:36:47 +0300

    collections

---
+++ src/sprint05/collections/Program.java
@@ -0,0 +1,16 @@
+package sprint05.collections;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Program {
+    Iterator<String> iterator;
+
+    Iterable<String> iterable;
+
+    List<String> list;
+
+    public static void main(String[] args) {
+
+    }
+}
