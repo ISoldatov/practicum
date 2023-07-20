ffddc35d05f2513bc05f8d1de2639a3d58a0e9e9
Ivan Soldatov <38758452+ivansoldatov@users.noreply.github.com>
2023-07-20 18:14:53 +0300

    start CandyResult

---
+++ src/sprint07/optional/candyresult/Practicum.java
@@ -0,0 +1,12 @@
+package sprint07.optional.candyresult;
+
+public class Practicum {
+    public static void main(String[] args) {
+        // Доработайте данный метод, чтобы на экран выводилось
+        // сообщение в соответствии с заданием
+
+        SearchService searchService = new SearchService();
+
+        searchService.search("Африка")
+    }
+}
