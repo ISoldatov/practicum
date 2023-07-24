f963cbf8d41aa7ecebd39a033e17d923523ba2ca
Ivan Soldatov <38758452+ivansoldatov@users.noreply.github.com>
2023-07-24 22:43:57 +0300

    time

---
+++ src/sprint07/time/letumbler/Practicum.java
@@ -0,0 +1,151 @@
+package sprint07.time.letumbler;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class Practicum {
+    // Задайте форматирование для времени и даты в формате часы:минуты день.месяц.год
+    // Пример - 12:15 02.11.21
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm dd.MM.yy");
+    // Задайте форматирование для времени в формате часы:минуты
+    public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
+    public static void main(String[] args) {
+        System.out.println("Тест №1:");
+        printFlightInformation(
+                "12:15 02.11.21",
+                "VKO",
+                "LED",
+                30,
+                1,
+                55
+        );
+
+        System.out.println("\nТест №2:");
+        printFlightInformation(
+                "14:00 03.10.21",
+                "SVX",
+                "VVO",
+                0,
+                9,
+                5
+        );
+
+        System.out.println("\nТест №3:");
+        printFlightInformation(
+                "06:00 12.12.21",
+                "DME",
+                "VVO",
+                0,
+                12,
+                0
+        );
+
+        System.out.println("\nТест №4:");
+        printFlightInformation(
+                "23:00 29.03.22",
+                "LED",
+                "SVX",
+                0,
+                2,
+                55
+        );
+
+
+    }
+
+    private static void printFlightInformation(
+            String formattedDepartureTime,
+            String departureAirportCode,
+            String arrivalAirportCode,
+            int delay,
+            int flightDurationHours,
+            int flightDurationMinutes
+    ) {
+        Airport departureAirport;
+        Airport arrivalAirport;
+        // С помощью класса AirportDatabase получите данные об аэропортах вылета и посадки.
+        // При получении исключения выведите сообщение исключения.
+        try {
+            departureAirport = AirportDatabase.getAirportByCode(departureAirportCode);
+            arrivalAirport = AirportDatabase.getAirportByCode(arrivalAirportCode);
+
+
+
+        // Создайте экземпляр ZonedDateTime с помощью formattedDepartureTime и зоны аэропорта вылета.
+        ZonedDateTime departure = ZonedDateTime.of(LocalDateTime.parse(formattedDepartureTime, DATE_TIME_FORMATTER), ZoneId.of(departureAirport.getZone()));
+
+        // Выведите информацию о том, между какими городами будет перелёт.
+        System.out.println("Ваш билет на рейс " + departureAirport.getCity() + " - " + arrivalAirport.getCity() + ": ");
+
+        // Найдите продолжительность полёта.
+        Duration flightDuration = Duration.between(departure.toLocalTime(), LocalTime.of(flightDurationHours, flightDurationMinutes));
+        // Найдите время прибытия с учётом зоны прилёта.
+        ZonedDateTime arrival = departure.plus(flightDuration).withZoneSameInstant(ZoneId.of(arrivalAirport.getZone()));
+
+        // Заполните данные для передачи в метод печати билета.
+        // Город вылета
+        String departureCity = departureAirport.getCityForTicket();
+        // Город прилёта
+        String arrivalCity = arrivalAirport.getCityForTicket();
+        // Отформатированное время прилёта
+        String formattedArrivalTime = DATE_TIME_FORMATTER.format(arrival);
+        // Только время вылета
+        String departureTimeOnly = TIME_FORMATTER.format(arrival.toLocalTime());
+
+        printTicket(
+                formattedDepartureTime,
+                departureAirportCode,
+                arrivalAirportCode,
+                departureCity,
+                arrivalCity,
+                formattedArrivalTime,
+                departureTimeOnly
+        );
+
+        // Добавьте проверку на случай задержки.
+        if (delay > 0) {
+            // Определите продолжительность задержки.
+            Duration delayDuration = Duration.between(departure.toLocalTime(), departure.toLocalTime().plusMinutes(delay));
+            // Вычислите время вылета с учётом задержки.
+            ZonedDateTime departureWithDelay = departure.plus(delayDuration);
+            // Вычислите время прилёта с учётом задержки.
+            ZonedDateTime arrivalWithDelay = departureWithDelay.plus(flightDuration).withZoneSameInstant(ZoneId.of(arrivalAirport.getZone()));
+
+            System.out.println("Ваш вылет задерживается.");
+            // Выведите продолжительность задержки в формате часы:минуты
+            System.out.println("Задержка: " + TIME_FORMATTER.format(LocalTime.of(0,delay)));
+            // Выведите отформатированное время вылета с учётом задержки.
+            System.out.println("Обновлённое время вылета: " + DATE_TIME_FORMATTER.format(departureWithDelay));
+            // Выведите отформатированное время прилёта с учётом задержки.
+            System.out.println("Обновлённое время прилёта: " + DATE_TIME_FORMATTER.format(arrivalWithDelay));
+        } else {
+            System.out.println("Удачного полёта!");
+        }
+        } catch (IllegalStateException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void printTicket(
+            String departureTime,
+            String departureAirportCode,
+            String arrivalAirportCode,
+            String departureCity,
+            String arrivalCity,
+            String arrivalTime,
+            String departureTimeOnly
+    ) {
+        System.out.println(
+                " _______________________________________________________\n" +
+                        "|                                            |          |\n" +
+                        "|  " + departureCity + "|" + departureAirportCode + "      "
+                        + departureTime + "  |   " + departureAirportCode + "    |\n" +
+                        "|  " + arrivalCity + "|" + arrivalAirportCode + "      "
+                        + arrivalTime + "  |   " + arrivalAirportCode + "    |\n" +
+                        "|                                            |          |\n" +
+                        "|  BOARDING TIME   --:--      SEAT  1A       |   " + departureTimeOnly + "  |\n" +
+                        "|  GATE  23                                  |   1A     |\n" +
+                        "|____________________________________________|__________|");
+    }
+}
