ec9f39c966afc4f074c8d364b42aa0e345be0d73
Ivan Soldatov <131395398+ISoldatov@users.noreply.github.com>
2023-07-25 17:00:13 +0300

    job

--- src/sprint07/junit/сookieаactory/FortuneCookieControllerTest.java
+++ src/sprint07/junit/сookieаactory/FortuneCookieControllerTest.java
@@ -1,5 +1,40 @@
 package sprint07.junit.сookieаactory;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
 public class FortuneCookieControllerTest {
+
+    static List<String> goodFortune = Collections.singletonList("positive!");
+    static List<String> badFortune = Collections.singletonList("negative");
+
+    static FortuneCookieController fortuneCookieControllerPositiv;
+    static FortuneCookieController fortuneCookieControllerNegativ;
+
+    @BeforeAll
+    public static void beforeAll() {
+        FortuneConfig configPositiv = new FortuneConfig(true);
+        FortuneConfig configNegativ = new FortuneConfig(false);
+        FortuneCookieFactory fortuneCookieFactoryPositiv = new FortuneCookieFactory(configPositiv, goodFortune, badFortune);
+        FortuneCookieFactory fortuneCookieFactoryNegativ = new FortuneCookieFactory(configNegativ, goodFortune, badFortune);
+        fortuneCookieControllerPositiv = new FortuneCookieController(fortuneCookieFactoryPositiv);
+        fortuneCookieControllerNegativ = new FortuneCookieController(fortuneCookieFactoryNegativ);
+    }
+
+    @Test
+    public void shouldReturnPositiveFortune() {
+        FortuneCookie fc= fortuneCookieControllerPositiv.tellFortune();
+        Assertions.assertEquals("positive!",fc.getFortuneText());
+    }
+
+    @Test
+    public void shouldReturnNegativeFortune () {
+        FortuneCookie fc= fortuneCookieControllerNegativ.tellFortune();
+        Assertions.assertEquals("negative",fc.getFortuneText());
+    }
 
 }
--- src/sprint07/junit/сookieаactory/FortuneCookieFactoryTest.java
+++ src/sprint07/junit/сookieаactory/FortuneCookieFactoryTest.java
@@ -1,4 +1,43 @@
 package sprint07.junit.сookieаactory;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
 public class FortuneCookieFactoryTest {
+
+    List<String> goodFortune = Collections.singletonList("Все будет хорошо!");
+    List<String> badFortune = Collections.singletonList("Все будет плохо!");
+    FortuneConfig fc = new FortuneConfig(true);
+    FortuneCookieFactory fcf;
+
+    @BeforeEach
+    public void beforeEach() {
+        fcf = new FortuneCookieFactory(fc, goodFortune, badFortune);
+    }
+
+    @Test
+    public void shouldIncrementCountByOneAfterOneCookieBaked() {
+        fcf.bakeFortuneCookie();
+        Assertions.assertEquals(1,fcf.getCookiesBaked());
+    }
+
+    @Test
+    public void shouldIncrementCountByTwoAfterTwoCookiesBaked () {
+        fcf.bakeFortuneCookie();
+        fcf.bakeFortuneCookie();
+        Assertions.assertEquals(2,fcf.getCookiesBaked());
+    }
+
+    @Test
+    public void shouldSetCounterToZeroAfterResetCookieCreatedCall() {
+        fcf.bakeFortuneCookie();
+        fcf.bakeFortuneCookie();
+        fcf.resetCookiesCreated();
+        Assertions.assertEquals(0,fcf.getCookiesBaked());
+    }
+
 }
